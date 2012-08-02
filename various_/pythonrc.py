69b15b3bf3caa06e27f5a3d460187e2fd5096ad2
Nolen Royalty <nolen.royalty@gmail.com>
2012-08-02 05:43:23 -0400

    add import operator and short function to time execution of a function

--- various_/pythonrc.py
+++ various_/pythonrc.py
@@ -7,3 +7,10 @@ import re
 import random
 import MySQLdb
 import subprocess
+import operator
+
+def timer(fn, *args):
+    "Time the application of fn to args. Return (result, seconds)."
+    import time
+    start = time.clock()
+    return fn(*args), time.clock() - start
