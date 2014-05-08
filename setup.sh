47e998f750c270f591f6c0e2569d34fc489759e1
Nolen Royalty <nolen.royalty@gmail.com>
2014-05-08 07:41:39 -0400

    simple shell install instead of messy python

--- setup.sh
+++ setup.sh
@@ -16,13 +16,24 @@ echo 'done'
 for dir in $DIRECTORIES; do
     echo "Setting up symlinks for all items in $dir"
     for file in $(ls $dir); do
+
+        if [ -L ~/.$file ]; then
+            echo "Removing symbolic link at ~/.$file"
+            rm ~/.$file # Just remove the file if it's already a symlink.  Prevents weird cyclical symlink bugs
+        fi
+
         if [ -e ~/.$file ]; then
             echo "Backing up $file"
             mv ~/.$file $BACKUP_DIR
         fi
+
         echo "Creating symlink for $file"
+        echo 'ln -s "$DOTFILES_DIR/$dir/$file" "~/.$file"'
         ln -s $DOTFILES_DIR/$dir/$file ~/.$file
+
     done
+    echo "Done setting up symlinks for $dir"
+    echo ''
 done
 
 touch ~/.dotfiles_config # Local machine settings for terminal
