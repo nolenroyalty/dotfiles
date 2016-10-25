33f16e8646df1fcdd641e17f39f3c30e189f685b
Nolen Royalty <nolen.royalty@gmail.com>
2016-10-24 20:46:26 -0400

    update twitch functions to use oauth

--- zsh/zsh/functions.zsh
+++ zsh/zsh/functions.zsh
@@ -4,17 +4,25 @@ fff () {
 }
 
 tt () {
-    livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best}
+    livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best} --twitch-oauth-token $TWITCH_TOKEN
 }
 
 rr () {
-    mkdir -p ~/Desktop/streams/$1; livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best} -o ~/Desktop/streams/$1/$1.mp4
+    mkdir -p ~/Desktop/streams/$1; livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best} -o ~/Desktop/streams/$1/$1.mp4 --twitch-oauth-token $TWITCH_TOKEN
 }
 
 ttv () {
-    livestreamer $1 ${2:=source} --player-passthrough=hls 
+    livestreamer $1 ${2:=source} --player-passthrough=hls --twitch-oauth-token $TWITCH_TOKEN
 }
 
 rrw () {
-    while true; do mkdir -p $HOME/desktop/streams/$1; mv $HOME/Desktop/streams/$1/$1.mp4 $HOME/Desktop/streams/$1/$1-`date "+%m-%d_%H-%M-%S"`.mp4; livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best} -o ~/Desktop/streams/$1/$1.mp4; sleep 30; done
+    while true; do mkdir -p $HOME/desktop/streams/$1; mv $HOME/Desktop/streams/$1/$1.mp4 $HOME/Desktop/streams/$1/$1-`date "+%m-%d_%H-%M-%S"`.mp4; livestreamer ${2:=twitch}.${4:=tv}/$1 ${3:=best} -o ~/Desktop/streams/$1/$1.mp4 --twitch-oauth-token $TWITCH_TOKEN; sleep 30; done
+}
+
+torclear () {
+    rm ~/Downloads/*.torrent
+}
+
+m3uclear () {
+    rm ~/Downloads/tors/*/*.m3u; rm ~/Downloads/tors/*/*.cue; rm ~/Downloads/tors/*/*.CUE; rm ~/Downloads/tors/*/*.log;
 }
